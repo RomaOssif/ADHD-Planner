b6bfafe2a53b243c36fe2b0f2b53da40695ccac8
Roman <roman.ossif256@gmail.com>
2023-10-22 21:36:36 +0200

    Implement version 1.
    Add:
    - UserControls folder
    - Task custom control
    - Introduce Workspace control
    - Implement some code in different places, mostly databindings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,23 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace ADHDPlanner
 {
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Task> tasks = new ObservableCollection<Task>();
+
+        public ObservableCollection<Task> Tasks
+        {
+            get { return tasks; }
+            set { tasks = value; }
+        }
+
         public MainWindow()
         {
+            DataContext = this;
+
             InitializeComponent();
         }
     }
---
+++ View/UserControls/Task.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ADHDPlanner.View.UserControls
+{
+    public partial class Task : UserControl, INotifyPropertyChanged
+    {
+        public Task()
+        {
+            DataContext = this;
+
+            InitializeComponent();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set 
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private TimeOnly estimatedTime;
+
+        public TimeOnly EstimatedTime
+        {
+            get { return estimatedTime; }
+            set 
+            {
+                estimatedTime = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        //ordered description?
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
