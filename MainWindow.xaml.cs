be8114e9c5d9d864f8e3a7a47e0549e128015cb1
Roman <roman.ossif256@gmail.com>
2023-10-31 21:41:42 +0100

    Refactor main window code and UI.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace ADHDPlanner
             tasks.Add(new UserControls.Task() { Title = "Task #1", EstimatedTime = "hh:mm:ss" });
 
             deleteTask.InputGestures.Add(new KeyGesture(Key.W, ModifierKeys.Control)); //CTRL + W = DELETE
-            saveTask  .InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control | ModifierKeys.Shift)); //CTRL + S = SAVE
+            saveTask.InputGestures  .Add(new KeyGesture(Key.S, ModifierKeys.Control)); //CTRL + S = SAVE
             createTask.InputGestures.Add(new KeyGesture(Key.T, ModifierKeys.Control)); //CTRL + T = CREATE
         }
 
@@ -47,14 +47,12 @@ namespace ADHDPlanner
         private void workspace_Click(object sender, RoutedEventArgs e)
         {
             if (e.OriginalSource.ToString() == "System.Windows.Controls.Button: X")
-            {
-                if (taskView.SelectedItem != null)
-                {
-                    Tasks.RemoveAt(taskView.SelectedIndex);
-                }
-            }
+                DeleteTask();
+        }
 
-            UpdateProgressBar();
+        private void AddTab_Click(object sender, RoutedEventArgs e)
+        {
+            AddTask();
         }
 
         private void UpdateProgressBar()
@@ -62,7 +60,7 @@ namespace ADHDPlanner
             progressBar.Text = $"{Tasks.Where(e => e.CurrentStage == Task.Stage.Finished).Count()} / {Tasks.Count}";
         }
 
-        private void AddTab_Click(object sender, RoutedEventArgs e)
+        private void AddTask()
         {
             Tasks.Add(new Task() { Title = "New task" });
 
@@ -74,20 +72,35 @@ namespace ADHDPlanner
             workspace.Title.Focus();
         }
 
-        private void DeleteTabCtrlW(object sender, RoutedEventArgs e)
+        private void DeleteTask()
         {
             if (taskView.SelectedItem != null)
             {
                 Tasks.RemoveAt(taskView.SelectedIndex);
-                
+
                 workspace.Set();
                 UpdateProgressBar();
             }
         }
 
+        private void taskView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (taskView.SelectedItem != null)
+            {
+                workspace.UpdateTask();
+
+                UpdateProgressBar();
+            }
+        }
+
+        private void DeleteTabCtrlW(object sender, RoutedEventArgs e)
+        {
+            DeleteTask();
+        }
+
         private void CreateTaskCtrlT(object sender, RoutedEventArgs e)
         {
-            AddTab_Click(sender, e);
+            AddTask();
         }
 
         private void SaveTaskCtrlS(object sender, RoutedEventArgs e)
@@ -101,15 +114,5 @@ namespace ADHDPlanner
         public static RoutedCommand deleteTask = new RoutedCommand();
         public static RoutedCommand saveTask = new RoutedCommand();
         public static RoutedCommand createTask = new RoutedCommand();
-
-        private void taskView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (taskView.SelectedItem != null)
-            {
-                workspace.UpdateTask();
-
-                UpdateProgressBar();
-            }
-        }
     }
 }
