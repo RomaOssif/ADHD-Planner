2e23f817273c45245d408d613a42316e719f1781
Roman <roman.ossif256@gmail.com>
2023-11-03 15:01:14 +0100

    Implement basic Pomodoro functionality.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using ADHDPlanner.View.UserControls;
+using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Timers;
 using System.Windows;
 using System.Windows.Input;
 using UserControls = ADHDPlanner.View.UserControls;
 
 namespace ADHDPlanner
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private ObservableCollection<UserControls.Task> tasks = new ObservableCollection<UserControls.Task>();
 
@@ -22,7 +26,45 @@ namespace ADHDPlanner
         public string TimerString
         {
             get { return timerString; }
-            set { timerString = value; }
+            set 
+            { 
+                timerString = value; 
+                OnPropertyChanged(); 
+            }
+        }
+
+        private System.Timers.Timer pomodoroTimer;
+        private TimeSpan timeout;
+        private bool? isRunning;
+
+        private void SetTimer(int miliseconds = 1000)
+        {
+            pomodoroTimer = new System.Timers.Timer(miliseconds);
+            timeout = new TimeSpan(0, 25, 1);
+
+            pomodoroTimer.AutoReset = true;
+            pomodoroTimer.Elapsed += OnTimeElapsed;
+
+            pomodoroTimer.Enabled = true;
+            isRunning = true;
+        }
+
+        private void OnTimeElapsed(object? source, ElapsedEventArgs e)
+        {
+            timeout -= new TimeSpan(0, 0, 1);
+
+            if (timeout > TimeSpan.Zero)
+                TimerString = timeout.ToString(@"mm\:ss");
+            else
+            {
+                TimerString = "Finished";
+                Application.Current.Dispatcher.BeginInvoke(() => btnStartTimer.Content = "Start");
+
+                isRunning = null;
+
+                pomodoroTimer.Stop();
+                pomodoroTimer.Dispose();
+            }
         }
 
         public MainWindow()
@@ -118,9 +160,40 @@ namespace ADHDPlanner
         public static RoutedCommand saveTask = new RoutedCommand();
         public static RoutedCommand createTask = new RoutedCommand();
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string  propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             AddTask();
+        }
+
+        private void btnStartTimer_Click(object sender, RoutedEventArgs e)
+        {
+            if (isRunning == null)
+            {
+                SetTimer();
+
+                btnStartTimer.Content = "Stop";
+            }
+            else if (isRunning == true)
+            {
+                pomodoroTimer.Stop();
+                
+                btnStartTimer.Content = "Resume";
+                isRunning = false;
+            }
+            else
+            {
+                pomodoroTimer.Start();
+
+                btnStartTimer.Content = "Stop";
+                isRunning = true;
+            }
         }
     }
 }
